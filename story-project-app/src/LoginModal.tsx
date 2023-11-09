854b173f7a5fb4a922c453c69c4f45d2ceeb1590
Tero Salmela <tero.salmela@reun.eu>
2023-11-09 15:44:04 +0200

    Modal rewrite done

--- story-project-app/src/LoginModal.tsx
+++ story-project-app/src/LoginModal.tsx
@@ -1,18 +1,12 @@
-import "./Modal.css";
-
-function LoginModal(props: { onClose: (on: boolean) => void }) {
+function LoginModal(props: { onSubmit: (event: React.FormEvent) => void }) {
   return (
-    <div className="modal" onClick={() => props.onClose(false)}>
-      <div className="modal-body" onClick={(e) => e.stopPropagation()}>
-        <form>
-          <label htmlFor="username">Username</label>
-          <input name="username" id="username" type="text"></input>
-          <label htmlFor="password">Password</label>
-          <input name="password" id="password" type="password"></input>
-          <button type="submit">Log in</button>
-        </form>
-      </div>
-    </div>
+    <form onSubmit={props.onSubmit}>
+      <label htmlFor="username">Username</label>
+      <input autoFocus name="username" id="username" type="text"></input>
+      <label htmlFor="password">Password</label>
+      <input name="password" id="password" type="password"></input>
+      <button type="submit">Log in</button>
+    </form>
   );
 }
 
---
+++ story-project-app/src/Modal.tsx
@@ -0,0 +1,66 @@
+import { useEffect, useRef, useState } from "react";
+import "./Modal.css";
+
+function Modal(props: {
+  isOpen: boolean;
+  hasCancelButton?: boolean;
+  onClose?: () => void;
+  children: React.ReactNode;
+}) {
+  const [modalOpen, setModalOpen] = useState(props.isOpen);
+  const modalRef = useRef<HTMLDialogElement | null>(null);
+
+  const handleCloseModal = () => {
+    if (props.onClose) {
+      props.onClose();
+    }
+    setModalOpen(false);
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLDialogElement>) => {
+    if (event.key === "Escape") {
+      handleCloseModal();
+    }
+  };
+
+  const handleBackgroundClose = (
+    event: React.MouseEvent<HTMLDialogElement>
+  ) => {
+    if (event.target === modalRef.current) {
+      handleCloseModal();
+    }
+  };
+
+  useEffect(() => {
+    setModalOpen(props.isOpen);
+    if (props.isOpen) {
+      document.body.style.overflow = "initial";
+    } else {
+      document.body.style.overflow = "visible";
+    }
+  }, [props.isOpen]);
+
+  useEffect(() => {
+    const modalElement = modalRef.current;
+    if (modalElement) {
+      if (modalOpen) {
+        modalElement.showModal();
+      } else {
+        modalElement.close();
+      }
+    }
+  }, [modalOpen]);
+
+  return (
+    <dialog
+      ref={modalRef}
+      onKeyDown={(e) => handleKeyDown(e)}
+      className="modal"
+      onClick={handleBackgroundClose}
+    >
+      {props.children}
+    </dialog>
+  );
+}
+
+export default Modal;
