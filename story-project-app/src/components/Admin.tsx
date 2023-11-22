65b50003066d28301392c8d9d96850bec9d60f36
Tero Salmela <tero.salmela@reun.eu>
2023-11-22 13:22:15 +0200

    Update Admin page

--- story-project-app/src/components/Admin.tsx
+++ story-project-app/src/components/Admin.tsx
@@ -4,7 +4,18 @@ function Admin() {
   return (
     <div className="content">
       <h2>Story Project Admin Panel</h2>
-      <p>Select a story to edit contents.</p>
+      <p>Welcome to the Story Project Admin panel.</p>
+      <ul>
+        <li>Select a story to edit contents.</li>
+        <li>
+          Press <span style={{ fontStyle: "italic" }}>Add New Story</span> to
+          create a new story.
+        </li>
+        <li>
+          Click the header logo to swap between public and private sides of the
+          site.
+        </li>
+      </ul>
     </div>
   );
 }
